7c51870a07f99a0894b3fcb0548d00ba28e1bf41
Hammad Shami <hammad.shami@sjsu.edu>
2023-07-21 18:39:45 -0700

    creating a function to install db locally

--- lib/components/business.dart
+++ lib/components/business.dart
@@ -1,6 +1,9 @@
 import 'dart:ffi';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:Ummah2U/components/location.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
+import 'dart:io';
 
 class Business {
   String name;
@@ -13,4 +16,48 @@ class Business {
 
   Business(this.name, this.location, this.phoneNo, this.email, this.rating,
       this.owner, this.website);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'lat': location.latitude,
+      'lng': location.longitude,
+      'phoneNo': phoneNo,
+      'email': email,
+      'rating': rating,
+      'owner': owner,
+      'website': website
+    };
+  }
+}
+
+Future<List<Business>> getBusinesses() async {
+  List<Business> retval = [];
+  var db = FirebaseFirestore.instance;
+  await db.collection("businesses").get().then((event) async {
+    for (var doc in event.docs) {
+      //create a business object and store a reference to it in retval
+      retval.add(Business(
+          doc.data()["name"],
+          Location(doc.data()["_geoloc"]["lat"], doc.data()["_geoloc"]["lng"]),
+          doc.data()["phoneNo"],
+          doc.data()["email"],
+          doc.data()["rating"],
+          doc.data()["owner"],
+          doc.data()["website"]));
+    }
+  });
+  writeBusinesses(retval);
+  return retval;
+}
+
+//take a list of businesses and write them to a json file
+Future<void> writeBusinesses(List<Business> businesses) async {
+  final directory = await getApplicationDocumentsDirectory();
+  String path = "${directory.path}\\assets\\businesses.json";
+  for (var business in businesses) {
+    final jsonData = jsonEncode(business.toJson());
+    File(path).writeAsStringSync(jsonData);
+  }
+  print("Write Complete");
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import cloud_firestore
 import firebase_core
 import geolocator_apple
 import package_info_plus
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
